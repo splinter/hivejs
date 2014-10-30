c1c54da1bdcf8a7cc33e567b40c1736b4780a3ab
sameeram <mgjaye@gmail.com>
2014-10-30 21:01:12 +0530

    First commit

---
+++ app/scripts/hive.js
@@ -0,0 +1,19 @@
+(function($,window){
+
+	var hive={};
+
+	hive.init=function(){
+
+	};
+
+	hive.start=function(){
+
+	};
+
+	hive.stop =function(){
+
+	};
+
+	window.hive=hive;
+
+}(jQuery,window));
